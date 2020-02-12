5453c31d6e69eb151d47fd5a2a0302323cb9d6d5
sadscv <sadscv@hotmail.com>
2020-02-12 15:38:40 +0800

    sync

--- app/form.py
+++ app/form.py
@@ -8,12 +8,12 @@
 @desc: 
 """
 from flask_wtf import Form
-from wtforms import StringField, PasswordField, BooleanField, SubmitField, SelectField
-from wtforms.validators import DataRequired, Length, Email
+from wtforms import PasswordField, BooleanField, SubmitField, SelectField
+from wtforms.validators import DataRequired
 
 
 class LoginForm(Form):
     name = SelectField('学院名称', choices=[], coerce=int)
-    password = PasswordField('Password', validators=[DataRequired()])
+    password = PasswordField('密码', validators=[DataRequired()])
     remember_me = BooleanField('保持登录')
-    submit = SubmitField('login')
+    submit = SubmitField('登录')
